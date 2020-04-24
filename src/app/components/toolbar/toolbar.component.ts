263b4afc5ed7d38dd38a7fb43ce150eaf13c3699
Arpit Satyal <arpited7@gmail.com>
2020-04-25 00:50:29 +0545

    show no of unread msgs

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TokenService } from 'src/app/services/token.service';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import * as M from 'materialize-css'
 import { UserService } from 'src/app/services/user.service';
 import { MomentService } from 'src/app/services/moment.service';
@@ -18,16 +18,23 @@ export class ToolbarComponent implements OnInit {
   notifications = []
   chatList = []
   noOfUnreadNotifs = 0
+  noOfUnreadMsg = 0
+  lastMsg
+  url
+  reciever
   constructor(
     public tokenService: TokenService,
     public router: Router,
     private userService: UserService,
-    private moment: MomentService
+    private moment: MomentService,
+    private activatedRoute: ActivatedRoute
   ) {
     this.socket = io(environment.server)
   }
 
   ngOnInit(): void {
+    this.url = this.router.url
+    this.reciever = `/chat/${this.activatedRoute.snapshot.params.name}`
     this.user = this.tokenService.GetPayload()
     let dropdownEl = document.querySelectorAll('.dropdown-trigger')
     M.Dropdown.init(dropdownEl, {
@@ -55,8 +62,22 @@ export class ToolbarComponent implements OnInit {
       this.notifications = res.notifications.reverse()
       this.notifications.forEach(n => n.read ? '' : this.noOfUnreadNotifs += 1)
       this.chatList = res.chatList
-      console.log(this.chatList)
+      this.checkIfRead(this.chatList).then(res => {
+        this.lastMsg = res
+       this.lastMsg.isRead ? '' : this.noOfUnreadMsg += 1
+      })
     })
+  }
+
+  checkIfRead(arr) {
+    return new Promise((res, rej) => {
+       arr.forEach(el => {
+      el.messageId.message.forEach((e, i) => {
+        if(i === el.messageId.message.length -1) res(e)
+      })
+    })
+    })
+   
   }
   timeFromNow(time) { return this.moment.timeFromNow(time) }
 
