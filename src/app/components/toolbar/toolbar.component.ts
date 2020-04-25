de6ef2001eb456fc3561bcb9785cab4698de4f99
Arpit Satyal <arpited7@gmail.com>
2020-04-25 18:27:22 +0545

    show recent msgs first

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { TokenService } from 'src/app/services/token.service';
-import { Router, ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
 import * as M from 'materialize-css'
 import { UserService } from 'src/app/services/user.service';
 import { MomentService } from 'src/app/services/moment.service';
 import io from 'socket.io-client'
 import { environment } from 'src/environments/environment';
+import _ from 'lodash'
 
 @Component({
   selector: 'app-toolbar',
@@ -18,23 +19,17 @@ export class ToolbarComponent implements OnInit {
   notifications = []
   chatList = []
   noOfUnreadNotifs = 0
-  noOfUnreadMsg = 0
-  lastMsg
-  url
-  reciever
+  noOfUnreadMsgs = 0
   constructor(
     public tokenService: TokenService,
     public router: Router,
     private userService: UserService,
-    private moment: MomentService,
-    private activatedRoute: ActivatedRoute
+    private moment: MomentService
   ) {
     this.socket = io(environment.server)
   }
 
   ngOnInit(): void {
-    this.url = this.router.url
-    this.reciever = `/chat/${this.activatedRoute.snapshot.params.name}`
     this.user = this.tokenService.GetPayload()
     let dropdownEl = document.querySelectorAll('.dropdown-trigger')
     M.Dropdown.init(dropdownEl, {
@@ -62,22 +57,20 @@ export class ToolbarComponent implements OnInit {
       this.notifications = res.notifications.reverse()
       this.notifications.forEach(n => n.read ? '' : this.noOfUnreadNotifs += 1)
       this.chatList = res.chatList
-      this.checkIfRead(this.chatList).then(res => {
-        this.lastMsg = res
-       this.lastMsg.isRead ? '' : this.noOfUnreadMsg += 1
-      })
+      console.log(this.chatList)
+      this.checkIfRead(this.chatList)
     })
   }
 
   checkIfRead(arr) {
-    return new Promise((res, rej) => {
-       arr.forEach(el => {
-      el.messageId.message.forEach((e, i) => {
-        if(i === el.messageId.message.length -1) res(e)
-      })
+    arr.forEach(el => {
+      let lastMsg = el.messageId.message[el.messageId.message.length - 1]
+      if(this.router.url !== `/chat/${lastMsg.senderName}`) {
+        if(!lastMsg.isRead && lastMsg.recieverName === this.user.username) {
+          this.noOfUnreadMsgs ++
+        }
+      } 
     })
-    })
-   
   }
   timeFromNow(time) { return this.moment.timeFromNow(time) }
 
