95fd4b17cc411888c7ca7e85525972a3aca5b44b
Arpit <arpited7@gmail.com>
2021-09-20 20:59:33 +0545

    prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+   backendUrl: 'https://memeclub.herokuapp.com/api/',
+  server: 'https://memeclub.herokuapp.com'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  backendUrl: 'http://localhost:8080/api/',
-  server: 'http://localhost:8080'
+  backendUrl: 'https://memeclub.herokuapp.com/api/',
+  server: 'https://memeclub.herokuapp.com'
 };
 
 /*
